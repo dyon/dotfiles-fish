528cb70c97e0519e0f2f332dfa26e2e289e0d944
Dyon <admin@skwow.net>
2014-11-16 21:59:35 +0000

    Fixed symlink mess...

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,1 +1,23 @@
-/Users/dani/Dropbox/Mackup/.config/fish/config.fish
+# Path to your oh-my-fish.
+set fish_path $HOME/.oh-my-fish
+set fish_config_path $HOME/.config/fish
+
+# Theme
+set fish_theme robbyrussell
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Example format: set fish_plugins autojump bundler
+
+# Set PATH variable
+set PATH /usr/local/bin /Users/$USER/.bin /usr/local/mysql/bin /usr/local/php5/bin /Users/$USER/composer-packages/vendor/bin $PATH
+
+# Path to your custom folder (default path is $FISH/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
+. $fish_config_path/.exports
+. $fish_config_path/.aliases
+
+source "$HOME/.homesick/repos/homeshick/homeshick.fish"
